2d4f672d97dfc274f62fa26a89cc6742e9aabb3e
Ahmed Elsayed <ahmedelsayed6179@gmail.com>
2025-12-27 20:07:15 +0200

    Add newReaction event to handle message reactions in socket connection

--- server.js
+++ server.js
@@ -127,6 +127,14 @@ io.on('connection', socket => {
     }
   });
 
+  this.socket.on('newReaction', data => {
+    const msg = this.messages.find(m => m.id === data.messageId);
+    if (msg) {
+      msg.reaction = data.reaction;
+      this.cd.detectChanges();
+    }
+  });
+
   socket.on('sendMessage', msg => {
     if (typeof msg !== 'string' || !msg.trim()) return;
     if (socket.room) {
