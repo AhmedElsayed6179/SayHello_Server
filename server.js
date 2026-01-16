5c540e00eb962f68b807fbf4d94dcefd8fc12596
Ahmed Elsayed <ahmedelsayed6179@gmail.com>
2026-01-16 23:12:23 +0200

    Update Server

--- server.js
+++ server.js
@@ -249,5 +249,4 @@ io.on('connection', socket => {
   });
 });
 
-const PORT = process.env.PORT || 8080;
-server.listen(PORT, () => console.log('Server running on port', PORT));
+module.exports = server;
